48361b0e61f17de156cff6d4070797d651d6d402
Abhishek Chaudhary <abhishekchoudharuy1810@gmail.com>
2023-04-11 14:29:46 +0530

    Binded both button state to parent

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import "./App.css";
 //import usestate for state management
 import { useState } from "react";
-
 //JSX :- A Html Tag inside a javascript function
 //Nesting One component into another
 //In order to differentiate between html tag and component you have to look at the
 //Capital letter in starting of a component
-function Button() {
-  //function to handle state Management
+function Button({count,onClick}) {
  //count to store value and setcount to update the value of this state
-  const [count, setCount] = useState(0);
-  function handleClick() {
-    setCount(count + 1);
-  }
-  return <button onClick={handleClick}>clicked me {count} times</button>;
+  return (
+  <button onClick={onClick}>clicked {count} times</button>
+  );
 }
 //created a object to fetch and store a dummy image data
 const Image_obj = {
@@ -23,13 +19,18 @@ const Image_obj = {
 
 // creation and initiliazation of Array of Objects
 const Phone = [
-  { name: "Oneplus", price: "40 thousands", id: "1" },
-  { name: "Apple", price: "50 thousand", id: "2" },
+  { name: "Oneplus", price: "40 thousands", id: 1 },
+  { name: "Apple", price: "50 thousand", id: 2 },
+  { name: "Samsung", price: "60 thousand", id: 3 },
+  { name: "Google", price: "70 thousand", id: 4 },
 ];
 
 function App() {
   //Iterate over an array of object using map , passed array as an argument to map and created a JSX component as list
-
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count+1);
+  }
   const Listitems = Phone.map((Phone) => (
     //List must have a key to differentiate between two child nodes
     <li className="Listitem" key={Phone.id}>
@@ -48,14 +49,13 @@ function App() {
           src={Image_obj.imageUrl}
         ></img>
         <br></br>
-        <Button />
-        <Button/>
+        <Button count={count+1} onClick={handleClick}/>
+        <Button count={count+1} onClick={handleClick} />
       </div>
       <div>
         <ul>{Listitems}</ul>
       </div>
     </div>
   );
-}
-
+  }
 export default App;
