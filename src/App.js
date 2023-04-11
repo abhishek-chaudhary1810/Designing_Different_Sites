0478a883418199d3670341290e415514be619b77
Abhishek Chaudhary <abhishekchoudharuy1810@gmail.com>
2023-04-11 10:43:32 +0530

    Rendered and displayed a list using an map

--- src/App.js
+++ src/App.js
@@ -3,25 +3,43 @@ import "./App.css";
 //Nesting One component into another
 //In order to differentiate between html tag and component you have to look at the
 //Capital letter in starting of a component
-function Button() { 
+function Button() {
   return <button>Click Me</button>;
 }
 //created a object to fetch and store a dummy image data
-const Image_obj={
+const Image_obj = {
   name: "Image show",
   imageUrl: "https://picsum.photos/200/300",
-
-}
+};
+const Phone = [
+  { name: "Oneplus", price: "40 thousands", id: "1" },
+  { name: "Apple", price: "50 thousand", id: "2" },
+];
 
 function App() {
+  const Listitems = Phone.map((Phone) => (
+    <li key={Phone.id}>
+      {"  "+ Phone.name}
+      {"  "+Phone.price}
+    </li>
+  ));
+  //console.log(Listitems);
   return (
     <div>
-      <h3> Hey there</h3>
-      {/* Below used external Css and value fetching in src with curly brackets */}
-      <img className="showimg" src={Image_obj.imageUrl} ></img>
-      <br></br>
-      <Button />
-          </div>
+      <div>
+        <h3> Hey there</h3>
+        {/* Below used external Css and value fetching in src with curly brackets */}
+        <img className="showimg" src={Image_obj.imageUrl}></img>
+        <br></br>
+        <Button />
+        <Button />
+      </div>
+      <div>
+        <ul>
+         {Listitems}
+        </ul>
+      </div>
+    </div>
   );
 }
 
